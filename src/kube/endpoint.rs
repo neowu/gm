5865a41ca2d74ed0781cd6b2b9cd6d73d0ee5219
neo <1100909+neowu@users.noreply.github.com>
2025-10-08 14:10:47 +0800

    kube, generate both endpoint and endpointslice

--- src/kube/endpoint.rs
+++ src/kube/endpoint.rs
@@ -28,7 +28,61 @@ metadata:
 addressType: IPv4
 endpoints:
 - addresses:
-  - {ip}"#
+  - {ip}
+---
+apiVersion: v1
+kind: Endpoints
+metadata:
+  name: {name}
+  namespace: {ns}
+subsets:
+- addresses:
+  - ip: {ip}"#
         )
+    }
+}
+
+mod test {
+    use crate::kube::endpoint::Endpoint;
+
+    #[test]
+    fn to_kube_config() {
+        let endpoint = Endpoint {
+            name: "db",
+            ns: "app",
+            ip: "10.10.0.1",
+        };
+
+        assert_eq!(
+            endpoint.to_kube_config(),
+            r#"apiVersion: v1
+kind: Service
+metadata:
+  name: db
+  namespace: app
+spec:
+  clusterIP: None
+---
+apiVersion: discovery.k8s.io/v1
+kind: EndpointSlice
+metadata:
+  name: db
+  namespace: app
+  labels:
+    kubernetes.io/service-name: db
+addressType: IPv4
+endpoints:
+- addresses:
+  - 10.10.0.1
+---
+apiVersion: v1
+kind: Endpoints
+metadata:
+  name: db
+  namespace: app
+subsets:
+- addresses:
+  - ip: 10.10.0.1"#
+        );
     }
 }
